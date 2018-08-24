6e8b60dac111adb4c246e8389fc275c69c4d6ed3
desrinivasan <desrinivasan@ebay.com>
2018-08-23 22:23:41 -0700

    create a simple docker image that displays date and time

---
+++ simpleusage/mydatetime.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+import datetime
+
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Date and time:</b> {time}<br/>" 
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), time=datetime.datetime.now())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
